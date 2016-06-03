06c356590a6a058237ddb4a9b32e5402a7660013
sbower <shawn.bower@gmail.com>
2016-06-02 20:50:12 -0400

    changing to the CS engine

--- cs-base/recipes/default.rb
+++ cs-base/recipes/default.rb
@@ -7,28 +7,26 @@
 # All rights reserved - Do Not Redistribute
 #
 
-
-package 'docker' do
-  action :install
-end
-
-service 'docker' do
-  action :start
-end
-
-docker_registry 'https://docker.cucloud.net/' do
-  username node['dtr']['user']
-  password node['dtr']['password']
-  email ''
-end
-
-docker_registry 'https://dtr.cucloud.net/' do
-  username node['dtr']['user']
-  password node['dtr']['password']
-  email ''
-end
-
 if node[:platform_family].include?("debian")
+
+  execute 'update_apt_keys' do
+    command "curl -s 'https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e' | sudo apt-key add --import"
+  end
+
+  package 'linux-image-extra-virtual' do
+    action :install
+  end
+
+  file '/etc/apt/sources.list.d/docker.list' do
+    content 'deb https://packages.docker.com/1.11/apt/repo ubuntu-trusty main'
+    mode '0644'
+    owner 'root'
+    group 'root'
+  end
+
+  apt_update 'name' do
+    action :update
+  end
   remote_file '/tmp/amazon-ssm-agent.deb' do
     source 'https://amazon-ssm-us-east-1.s3.amazonaws.com/latest/debian_amd64/amazon-ssm-agent.deb'
     action :create
@@ -54,4 +52,24 @@ else
   file '/tmp/amazon-ssm-agent.rpm' do
     action :delete
   end
+end
+
+package 'docker-engine' do
+  action :install
+end
+
+group 'docker' do
+  action :modify
+  members 'srb55'
+  append true
+end
+
+service 'docker' do
+  action :start
+end
+
+docker_registry 'https://dtr.cucloud.net/' do
+  username node['dtr']['user']
+  password node['dtr']['password']
+  email ''
 end
